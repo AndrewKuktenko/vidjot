714d9b8fb011d5f8de8370c5aa80a29ff9e71209
AndrewKuktenko <andriy.kuktenko@gmail.com>
2018-06-05 19:07:43 +0300

    mongodb set

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev')
+	.then(() => console.log('MongoDB connected...'))
+	.catch((error) => console.log(error));
+
+//Load Idea Module
+require('./models/Idea')
+
+const Idea = mongoose.model('ideas');
 
 app.engine('handlebars', exphbs({
 	defaultLayout:'main'
@@ -23,4 +34,5 @@ const port = 5000;
 
 app.listen(port, () => {
 	console.log(`Server started at port ${port}`);
-})
+});
+
